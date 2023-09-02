b7529705b19e8d99735c04dc2f47594d5161dd9d
Felix Paniagua <fpg72396@gmail.com>
2023-09-02 17:49:08 -0400

    Fixing bugs and programming textareas

--- assets/js/digits.js
+++ assets/js/digits.js
@@ -1,38 +1,16 @@
-const $randomNumberContainer = document.querySelector("#random-number"),
-    $randomNumberContainer2 = document.querySelector("#random-number2"),
-    $btnSwitch = document.querySelector("#btn-switch"),
-    $btnGenerate = document.querySelector("#btn-generate"),
-    $btnGenerate2 = document.querySelector("#btn-generate2"),
-    $btnClear = document.querySelector("#btn-clear"),
-    $btnClear2 = document.querySelector("#btn-clear2"),
-    $btnRestart = document.querySelector("#btn-restart"),
-    $btnTest = document.querySelector("#btn-test"),
-    $btnTest2 = document.querySelector("#btn-test2"),
-    $timeRecord = document.querySelector("#time-record"),
-    $digitRecallGameContainer = document.querySelector(".main__content-container.digit-recall-game"),
-    $reversedDigitRecallGameContainer = document.querySelector(".main__content-container.reversed-digit-recall-game")
-
-
-let randomFigureString = "",
-    recalledFigureString = "",
-    started = false,
-    gameIndex = 0,
-    timeRecord = localStorage.getItem("time")
-
-
 
 $timeRecord.textContent = timeRecord || "00:00:00"
 
 function showXs() {
     if (!started) return
-    if (gameIndex === 1 && $flashModeInput.checked) return
+    if (gameIndex === 1 && flashMode) return
 
-    if (gameIndex === 0) $randomNumberContainer.innerHTML = ""
-    else $randomNumberContainer2.innerHTML = ""
+    $randomNumberContainer.innerHTML = ""
+    $randomNumberContainer2.innerHTML = ""
 
-    let len = parseInt($digitAmountInput2.value)
+    let length = gameIndex === 0 ? digitAmount : digitAmount2
 
-    for (let index = 0; index < len; index++) {
+    for (let index = 0; index < length; index++) {
         if (gameIndex === 0) $randomNumberContainer.innerHTML += "X "
         else $randomNumberContainer2.innerHTML += "X ";
     }
@@ -52,17 +30,17 @@ $randomNumberContainer.onmouseover = () => {
 
 $randomNumberContainer2.onmouseover = () => {
     if (!started) return
-    if (gameIndex === 1 && $flashModeInput.checked) return
+    if (gameIndex === 1 && flashMode) return
     let html = showRandomFigure()
     $randomNumberContainer2.innerHTML = html
 }
 
 
 
-function generateRandomFigure(len) {
+function generateRandomFigure(length) {
     randomFigureString = ""
-    for (let index = 0; index < len; index++) {
-        let randomNumber = getRandomNumber();
+    for (let index = 0; index < length; index++) {
+        let randomNumber = getRandomNumber()
         randomFigureString += randomNumber
     }
 }
@@ -99,8 +77,7 @@ $btnSwitch.onclick = () => {
 }
 
 $btnGenerate.onclick = () => {
-    let len = parseInt($digitAmountInput.value)
-    generateRandomFigure(len)
+    generateRandomFigure(digitAmount)
 
     let html = showRandomFigure()
     $randomNumberContainer.innerHTML = html
@@ -112,28 +89,30 @@ $btnGenerate.onclick = () => {
 }
 
 $btnGenerate2.onclick = () => {
-    let len = parseInt($digitAmountInput2.value)
-    generateRandomFigure(len)
+    $btnClear2.click()
 
-    
-    if ($flashModeInput.checked) {
-        let milliseconds = 1000 / $speed.value
+    generateRandomFigure(digitAmount2)
 
-        let index = 0
+
+    if (flashMode) {
         if (!started) {
-            time2 = setInterval(() => {
+            let milliseconds = 1000 / speed,
+                index = 0
 
+            started = true
+            time2 = setInterval(() => {
                 $randomNumberContainer2.innerHTML = `<span class="main__random-digit reversed">${randomFigureString[index++]}</span>`
 
-                if (index === len) {
+                if (index === digitAmount2) {
                     clearInterval(time2)
                     started = false
+                    index = 0
                 }
             }, milliseconds)
-            started = true
+
         }
 
-        
+
     }
     else {
         let html = showRandomFigure()
@@ -143,29 +122,33 @@ $btnGenerate2.onclick = () => {
 
 $btnClear.onclick = () => {
     recalledFigureString = ""
+    $textArea1.value = ""
     showRecalledFigure()
 }
 
 $btnClear2.onclick = () => {
     recalledFigureString = ""
+    $textArea2.value = ""
     showRecalledFigure()
 }
 
 
 
 $btnRestart.onclick = () => {
+    if (!started) return
 
-   if(!saved)
-   {
-    let ok = confirm("Are you sure you want to restart?")
-    if(!ok) return
-   }
-   else{
-    saved = false
-   }
+    if (!restart) {
+        let ok = confirm("Are you sure you want to restart?")
+        if (!ok) return
+    }
+    else {
+        restart = false
+    }
 
-    if (started)
-        clearInterval(time1)
+
+
+
+    clearInterval(time1)
 
     showXs()
     hours = 0
@@ -173,8 +156,11 @@ $btnRestart.onclick = () => {
     seconds = 0
     figureCounter = 0
     started = false
+
     $timeSpan.textContent = `${getFormattedDigits(hours)}:${getFormattedDigits(minutes)}:${getFormattedDigits(seconds)}`
-    $figureCounterSpan.textContent = `0/${setting.figure_amount}`
+    $figureCounterSpan.textContent = `0/${figureAmount}`
+    $textArea1.value = ""
+    $textArea2.value = ""
     spansTemplate = ""
     recalledFigureString = ""
     randomFigureString = ""
@@ -182,7 +168,11 @@ $btnRestart.onclick = () => {
     showRecalledFigure()
 }
 
+$textArea1.oninput = () => recalledFigureString = $textArea1.value
+$textArea2.oninput = () => recalledFigureString = $textArea2.value
+
 $btnTest.onclick = () => {
+
     if (recalledFigureString === "") return
 
     if (randomFigureString === recalledFigureString) {
@@ -190,7 +180,7 @@ $btnTest.onclick = () => {
         showRecalledFigure()
 
         attemptCounter++
-        if (attemptCounter === setting.attempt_amount) {
+        if (attemptCounter === attemptAmount) {
             winLossTrackerArray.push(1)
             checkRecord()
 
@@ -200,13 +190,14 @@ $btnTest.onclick = () => {
             $btnGenerate.click()
         }
 
-        $figureCounterSpan.textContent = `${figureCounter}/${setting.figure_amount}`
-        $attemptCounterSpan.textContent = `${attemptCounter}/${setting.attempt_amount}`
+        $figureCounterSpan.textContent = `${figureCounter}/${figureAmount}`
+        $attemptCounterSpan.textContent = `${attemptCounter}/${attemptAmount}`
 
-        if (figureCounter === setting.figure_amount) {
+        if (figureCounter === figureAmount) {
             checkTimeRecord()
 
             alert("Congratulations!")
+            restart = true
 
             $btnRestart.click()
         }
@@ -215,124 +206,129 @@ $btnTest.onclick = () => {
     else {
         alert("Try it again 😥")
     }
+
+    $btnClear.click();
 }
 
 $btnTest2.onclick = () => {
     if (recalledFigureString === "") return
 
-    randomFigureString = randomFigureString.split("").reverse().join("")
+    let reversedFigureString = randomFigureString.split("").reverse().join("")
 
-    if (randomFigureString === recalledFigureString) {
+    console.log(reversedFigureString, recalledFigureString)
+
+    if (reversedFigureString === recalledFigureString) {
         recalledFigureString = ""
         showRecalledFigure()
 
         alert("Congratulations!")
 
         $btnGenerate2.click()
-        $btnClear2.click()
 
-        
+        restart = false
+
+
     }
-    else{
+    else {
         alert("Try it again 😥")
     }
+    $btnClear2.click()
 
 }
 
-    window.onkeyup = (e) => {
+window.onkeyup = (e) => {
 
-        let pattern = /Backspace|[\dsgcrt]/
+    let pattern = /Backspace|[\dsgcrt]/
 
 
 
-        if (gameIndex === 0) {
-            if (e.key == "s") {
-                $btnSave2.click()
-            }
-
-            if (!pattern.test(e.key) || started === false && e.key !== "g")
-                return
-
-            switch (e.key) {
-                case "g":
-
-                    $btnGenerate.click()
-                    break
-
-                case "c":
-                    $btnClear.click()
-                    break
-
-                case "r":
-                    $btnRestart.click()
-                    break
-
-                case "t":
-                    $btnTest.click()
-                    break
-
-                case "Backspace":
-                    if (recalledFigureString !== "")
-                        recalledFigureString = recalledFigureString.slice(0, -1)
-                    break
-
-                default:
-                    if (recalledFigureString.length < setting.digit_amount) {
-                        recalledFigureString += e.key
-                    }
-
-            }
-
-            showRecalledFigure()
+    if (gameIndex === 0) {
+        if (e.key == "s") {
+            $btnSave2.click()
         }
-        else if (gameIndex === 1) {
 
-            if (e.key == "s") {
-                $btnSave2.click()
-            }
+        if (!pattern.test(e.key) || started === false && e.key !== "g")
+            return
 
-            pattern = /Backspace|[\dgct]/
-            if (!pattern.test(e.key)) return
+        switch (e.key) {
+            case "g":
 
-            switch (e.key) {
+                $btnGenerate.click()
+                break
 
+            case "c":
+                $btnClear.click()
+                break
 
-                case "g":
+            case "r":
+                $btnRestart.click()
+                break
 
-                    $btnGenerate2.click()
-                    break
+            case "t":
+                $btnTest.click()
+                break
 
-                case "c":
-                    $btnClear2.click()
-                    break
+            case "Backspace":
+                if (recalledFigureString !== "")
+                    recalledFigureString = recalledFigureString.slice(0, -1)
+                break
 
-                case "t":
-                    $btnTest2.click()
-                    break
-
-                case "Backspace":
-                    if (recalledFigureString !== "")
-                        recalledFigureString = recalledFigureString.slice(0, -1)
-                    break
-
-                default:
-                    if (recalledFigureString.length < setting2.digit_amount) {
-                        recalledFigureString += e.key
-                    }
-
-            }
-            showRecalledFigure()
+            default:
+                if (recalledFigureString.length < digitAmount) {
+                    recalledFigureString += e.key
+                }
 
         }
+
+        showRecalledFigure()
     }
+    else if (gameIndex === 1) {
 
-    function showRecalledFigure() {
-        const $inputContainer = gameIndex === 0 ? document.querySelector("#input-container") : document.querySelector("#input-container2")
-        $inputContainer.innerHTML = ""
-        let len = recalledFigureString.split("").length
-
-        for (let index = 0; index < len; index++) {
-            $inputContainer.innerHTML += `<span class="main__random-digit">${recalledFigureString[index]}</span>`
+        if (e.key == "s") {
+            $btnSave2.click()
         }
+
+        pattern = /Backspace|[\dgct]/
+        if (!pattern.test(e.key) && started) return
+
+        switch (e.key) {
+            case "g":
+
+                $btnGenerate2.click()
+                break
+
+            case "c":
+                $btnClear2.click()
+                break
+
+            case "t":
+                $btnTest2.click()
+                break
+
+            case "Backspace":
+                if (recalledFigureString !== "")
+                    recalledFigureString = recalledFigureString.slice(0, -1)
+                break
+
+            default:
+                if (recalledFigureString.length < digitAmount2) {
+                    recalledFigureString += e.key
+                }
+                break
+
+        }
+        showRecalledFigure()
 
     }
+}
+
+function showRecalledFigure() {
+    const $inputContainer = gameIndex === 0 ? document.querySelector("#input-container") : document.querySelector("#input-container2")
+    $inputContainer.innerHTML = ""
+    let len = recalledFigureString.split("").length
+
+    for (let index = 0; index < len; index++) {
+        $inputContainer.innerHTML += `<span class="main__random-digit">${recalledFigureString[index]}</span>`
+    }
+
+}
--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,68 +1,91 @@
-const $digitAmountInput = document.querySelector("#digit-amount"),
-$digitAmountInput2 = document.querySelector("#digit-amount2"),
-$figureAmountInput = document.querySelector("#figure-amount"),
-$attemptAmountInput = document.querySelector("#attempt-amount"),
-$flashModeInput = document.querySelector("#flash-mode"),
-$speed = document.querySelector("#speed"),
-$btnSave = document.querySelector("#btn-save"),
-$btnSave2 = document.querySelector("#btn-save2")
 
-const setting = JSON.parse(localStorage.getItem("setting")) || {},
-setting2 = JSON.parse(localStorage.getItem("setting2")) || {}
+const setting = JSON.parse(localStorage.getItem("setting")) || {game1:{}, game2:{}}
 
-let saved = false
-if(Object.keys(setting).length > 0)
-{
-    $digitAmountInput.value = setting.digit_amount
-    $figureAmountInput.value = setting.figure_amount
-    $attemptAmountInput.value = setting.attempt_amount
+
+if (Object.keys(setting.game1).length > 0) {
+    $digitAmountInput.value = setting.game1.digit_amount
+    $figureAmountInput.value = setting.game1.figure_amount
+    $attemptAmountInput.value = setting.game1.attempt_amount
 }
 
-if(Object.keys(setting2).length > 0)
-{
-    $digitAmountInput2.value = setting2.digit_amount
-    $speed.value = setting2.speed
-    $flashModeInput.checked = setting2.flash_mode
-    if(!setting2.flash_mode) $speed.disabled = true
+if (Object.keys(setting.game2).length > 0) {
+    $digitAmountInput2.value = setting.game2.digit_amount
+    $speed.value = setting.game2.speed
+    $flashModeInput.checked = setting.game2.flash_mode
+    if (!setting.game2.flash_mode) $speed.disabled = true
     else $speed.disabled = false
 }
 
-$flashModeInput.onchange = () => {
-    if($flashModeInput.checked) $speed.disabled = false
-    else $speed.disabled = true
+
+
+
+
+function updateSettings()
+{
+    digitAmount = parseInt($digitAmountInput.value)
+    digitAmount2 = parseInt($digitAmountInput2.value)
+    figureAmount = parseInt($figureAmountInput.value)
+    attemptAmount = parseInt($attemptAmountInput.value)
+    speed = parseFloat($speed.value)
+
+    flashMode = $flashModeInput.checked
+    if ($flashModeInput.checked) 
+    $speed.disabled = false
+else  $speed.disabled = true
+
+    $textArea1.maxLength = digitAmount
+    $textArea2.maxLength = digitAmount2
 }
 
+updateSettings()
+
+$digitAmountInput.onchange = updateSettings
+
+$digitAmountInput2.onchange = updateSettings
+
+$figureAmountInput.onchange = updateSettings
+
+$attemptAmountInput.onchange = updateSettings
+
+$speed.onchange = updateSettings
+
+$flashModeInput.onchange = updateSettings
+
 $btnSave.onclick = () => {
-    if(started)
-    {
+    if (started) {
         let ok = confirm("Saving will automatically restart all your current progress. Are you sure you want to continue?")
-        if(!ok) return
+        if (!ok) return
     }
 
-    setting.digit_amount = parseInt($digitAmountInput.value)
-    setting.figure_amount = parseInt($figureAmountInput.value)
-    setting.attempt_amount = parseInt($attemptAmountInput.value)
+    updateSettings()
+
+
+    setting.game1.digit_amount = parseInt(digitAmount)
+    setting.game1.figure_amount = parseInt(figureAmount)
+    setting.game1.attempt_amount = parseInt(attemptAmount)
 
     localStorage.setItem("setting", JSON.stringify(setting))
 
-    $figureCounterSpan.textContent = `0/${setting.figure_amount||0}`
-    $attemptCounterSpan.textContent = `0/${setting.attempt_amount||0}`
+    $figureCounterSpan.textContent = `0/${figureAmount || 0}`
+    $attemptCounterSpan.textContent = `0/${attemptAmount || 0}`
 
     alert(`Setting Saved!`)
 
-    saved = true
+    restart = true
     $btnRestart.click()
 }
 
 
 $btnSave2.onclick = () => {
-    setting2.digit_amount = parseInt($digitAmountInput2.value)
-    setting2.speed = parseInt($speed.value)
-    setting2.flash_mode = $flashModeInput.checked
+    updateSettings()
 
-    if(!setting2.flash_mode) $speed.disabled = true
+    setting.game2.digit_amount = digitAmount2
+    setting.game2.speed = speed
+    setting.game2.flash_mode = flashMode
+
+    if (!flashMode) $speed.disabled = true
     else $speed.disabled = false
-    localStorage.setItem("setting2", JSON.stringify(setting2))
+    localStorage.setItem("setting", JSON.stringify(setting))
     alert(`Setting Saved!`)
 }
 

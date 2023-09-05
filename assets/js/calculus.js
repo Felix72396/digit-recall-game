daafdd5bb3db497d3a6083c1c4efedf1d364cfa7
Felix Paniagua <fpg72396@gmail.com>
2023-09-05 13:14:22 -0400

    adding an input to the third game

--- assets/js/calculus.js
+++ assets/js/calculus.js
@@ -1,6 +1,20 @@
 (() => {
-    let $td
+    let $td,
+        timeout = false,
+        result
 
+
+    const time = function () { timer = setInterval(getTime, 1000) }
+    const $timeSpan = document.querySelector(".super-recall-culus-game .main__time"),
+        $btnHint = document.querySelector("#btn-hint"),
+        $operationContainer = document.querySelector(".main__operation")
+
+    const operation = {
+        0:"+",
+        1:"-",
+        2:"x",
+        3:"/"
+    }
     function clear() {
         const $tdList = document.querySelectorAll("td")
         $tdList.forEach(td => {
@@ -9,20 +23,56 @@
         })
     }
 
-    const time = function () { timer = setInterval(getTime, 1000) }
-    const $timeSpan = document.querySelector(".super-recall-culus-game .main__time"),
-    $btnHint = document.querySelector("#btn-hint")
+    function performOperation() {
+
+        let a = Math.round(Math.random() * 100) + 1,
+            b = Math.round(Math.random() * 100) + 1,
+            randomOption = Math.round(Math.random() * 3)
+        
+        switch (randomOption) {
+            case 0:
+                result = a + b
+                break
+            case 1:
+                result = a - b
+                break
+            case 2:
+                result = a * b
+                break
+
+            case 3:
+                result = Math.round((a / b) * 10) / 10
+                console.log(result)
+                break
+        }
+
+        $operationContainer.innerHTML = `<div>
+            <span class="main__operation-value">${a}</span>
+            <span class="main__operation-symbol">${operation[randomOption]}</span>
+            <span class="main__operation-value">${b}</span>
+        </div> <input class="main__operation-input" type="text" maxlength="5" placeholder="Type the result">`
+
+        document.querySelector(".main__operation-input").onkeypress = (e) => {
+            let pattern = /[\d.]/
+        
+            if(!pattern.test(e.key)) 
+                e.preventDefault()
+        }
+    }
+
+
 
     function getTime() {
-        seconds--
-        
         $timeSpan.textContent = `${getFormattedDigits(seconds)} seg`
+        seconds--
 
-        if (seconds === 0) {
+        if (seconds === -1) {
             clearInterval(timer)
             const $parent = $timeSpan.parentElement
             $parent.classList.add("d-none")
             $btnHint.classList.remove("d-none")
+            performOperation()
+            timeout = true
             clear()
         }
     }
@@ -35,6 +85,7 @@
     }
 
     window.onclick = (e) => {
+        const $calculationContainer = document.querySelector(".main__calculation")
 
         if (e.target.matches("td")) {
             $digitContainer.style.display = "flex"
@@ -56,16 +107,23 @@
         }
 
         if (e.target.matches(".super-recall-culus-game .main__btn.generate")) {
-            if (!started) {
+            if (!picked) {
                 alert("Pick a matrix size")
                 return
             }
 
+            if (started) {
+                clearInterval(timer)
+            }
+
+            started = true
+            timeout = false
             const $parent = $timeSpan.parentElement
             $parent.classList.remove("d-none")
             $btnHint.classList.add("d-none")
             seconds = 60
             time()
+
 
             $tBody.innerHTML = ""
             for (let row = 0; row < matrixSize; row++) {
@@ -80,6 +138,8 @@
         }
 
         if (e.target.matches(".super-recall-culus-game .main__btn.test")) {
+            if (!timeout) return
+
             const $tdList = document.querySelectorAll("td")
 
             for (const td of $tdList) {
@@ -112,8 +172,52 @@
             e.target.classList.add("blue-button")
             matrixSize = parseInt(e.target.getAttribute("data-matrix-size"))
 
-            started = true
+            picked = true
+        }
+
+        if (e.target.matches("[data-matrix-size]")) {
+            const $btnList = document.querySelectorAll("[data-matrix-size]")
+
+            $btnList.forEach(btn => btn.classList.remove("blue-button"))
+
+            e.target.classList.add("blue-button")
+            matrixSize = parseInt(e.target.getAttribute("data-matrix-size"))
+
+            picked = true
+        }
+
+        if (e.target.matches("#btn-hint")) {
+            $calculationContainer.classList.remove("d-none")
+            performOperation()
+        }
+
+        if(e.target.matches(".main__btn.accept"))
+        {
+            let value = parseInt(document.querySelector(".main__operation-input").value)
+            if(value === result)
+            {
+                $calculationContainer.classList.add("d-none")
+                const $tdList = document.querySelectorAll("td")
+
+                $tdList.forEach(td => td.textContent = td.dataset.number)
+
+                timeout = false
+                const $parent = $timeSpan.parentElement
+                $parent.classList.remove("d-none")
+                $btnHint.classList.add("d-none")
+                seconds = 60
+                time()
+            }
+            else{
+                alert("Try it again😥!")
+            }
+        }
+
+        if (e.target.matches(".main__btn.cancel")) {
+            $calculationContainer.classList.add("d-none")
         }
 
     }
+
+    
 })()

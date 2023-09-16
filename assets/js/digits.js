b97d80e9ed01d3b7ef9b7a41934e9db68ba56555
Felix Paniagua <fpg72396@gmail.com>
2023-09-16 14:28:36 -0400

    Adding touchstart event

--- assets/js/digits.js
+++ assets/js/digits.js
@@ -240,6 +240,7 @@ function restart() {
     randomFigureString = ""
     // winLossTrackerArray = []
     // showRecalledFigure()
+
     clearInterval(time1)
 }
 
@@ -351,18 +352,21 @@ $textArea2.onkeypress = (e) => {
 }
 
 $textArea1.addEventListener("touchstart", () => {
-    alert("d")
+    showXs()
+    $btnShow.classList.remove("d-none")
+})
+
+$textArea2.addEventListener("touchstart", () => {
+    showXs()
+    $btnShow2.classList.remove("d-none")
 })
 
 $textArea1.oninput = () => {
     recalledFigureString = $textArea1.value
-    showXs()
-    $btnShow.classList.remove("d-none")
+   
 }
 $textArea2.oninput = () => {
     recalledFigureString = $textArea2.value
-    showXs()
-    $btnShow2.classList.remove("d-none")
 }
 
 window.onkeyup = (e) => {
